06b4ae10d56c9dccf1c4327540c0e60a6af886c2
Alex Lamson <alex.lamson@gmail.com>
2017-11-15 12:56:52 -0500

    forgot to add class labels to pickled features :flushed:

--- src/features_bow.py
+++ src/features_bow.py
@@ -3,6 +3,7 @@ from __future__ import nested_scopes, generators, division, absolute_import, wit
 from gensim.corpora.dictionary import Dictionary
 import pandas as pd
 import logistics_pickler
+from tqdm import tqdm
 
 
 def apply_feature_representation(in_file, out_file):
@@ -17,26 +18,37 @@ def apply_feature_representation(in_file, out_file):
     mydict = Dictionary([["<UNK>"]])
     mydict.add_documents(train_documents)
 
+    known_words = set(mydict.values())
+
     def word_is_known(word):
-        return word in mydict.values()
+        return word in known_words
 
     def filter_unknowns_in_doc(doc):
         return [word if word_is_known(word) else "<UNK>" for word in doc]
 
-    print("    processing all documents")
-    all_feature_vectors = []
-    for doc in train_documents:
+    X = []
+    Y = []
+
+    print("    counting number of rows")
+    num_rows = sum(1 for line in open(in_file))
+
+    print("    converting to feature representation")
+    for i, row in tqdm(df.iterrows(), total=num_rows):
+        doc = row['text'].split()
+        subreddit = row['subreddit']
+
         doc = filter_unknowns_in_doc(doc)
         features = mydict.doc2bow(doc)
-        all_feature_vectors.append(features)
+        X.append(features)
+        Y.append(subreddit)
 
     print("    saving file")
-    logistics_pickler.save_obj(all_feature_vectors, out_file, print_debug_info=False)
+    logistics_pickler.save_obj((X, Y), out_file, print_debug_info=False)
 
 
 def main():
-    apply_feature_representation("../res/data_all.csv", "../pickle_files/feature_all_bow.pkl")
     apply_feature_representation("../res/data_sample.csv", "../pickle_files/feature_sample_bow.pkl")
+    apply_feature_representation("../res/data_all.csv", "../pickle_files/feature_all_bow.pkl")
     apply_feature_representation("../res/data_all_train.csv", "../pickle_files/feature_all_train_bow.pkl")
     apply_feature_representation("../res/data_all_test.csv", "../pickle_files/feature_all_test_bow.pkl")
     apply_feature_representation("../res/data_all_validate.csv", "../pickle_files/feature_all_validate_bow.pkl")
