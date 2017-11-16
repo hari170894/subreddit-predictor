62cecf585c391e4c287b6bb0ee2598d9e709017d
Alex Lamson <alex.lamson@gmail.com>
2017-11-16 13:00:36 -0500

    added baseline model which predicts most common class :bowling:

---
+++ src/model_baseline.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2
+from __future__ import nested_scopes, generators, division, absolute_import, with_statement, print_function, unicode_literals
+import logistics_pickler
+from sklearn.metrics import accuracy_score
+
+print("loading pickled feature representation")
+X_train, Y_train = logistics_pickler.load_obj("../pickle_files/feature_all_train_bow.pkl")
+X_test, Y_test = logistics_pickler.load_obj("../pickle_files/feature_all_test_bow.pkl")
+
+print("training classifier")
+most_common_class = max(set(Y_train), key=list(Y_train).count)
+
+print("evaluating model")
+y_true = Y_test
+y_pred = [most_common_class] * len(y_true)
+accuracy = accuracy_score(y_true, y_pred)
+print("accuracy: {}".format(accuracy))
