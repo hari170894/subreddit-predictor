0417373d4ad23ecc384abb71465a98e8000d0bb8
Alex Lamson <alex.lamson@gmail.com>
2017-11-16 12:42:48 -0500

    made all vectors relative to training data :art:

--- src/features_bow.py
+++ src/features_bow.py
@@ -7,62 +7,74 @@ from tqdm import tqdm
 import numpy as np
 
 
-def apply_feature_representation(in_file, out_file):
-    print("converting file to BOW format {} -> {}".format(in_file, out_file))
-
-    print("    reading csv")
-    df = pd.read_csv(in_file, header=0)
+def make_dictionary_and_known_words(training_filename):
+    print("reading training csv")
+    df = pd.read_csv(training_filename, header=0)
 
     # Create dict that maps from words to IDs
-    print("    creating dictionary")
+    print("creating dictionary")
     train_documents = (x.split() for x in df['text'])
-    mydict = Dictionary([["<UNK>"]])
-    mydict.add_documents(train_documents)
+    dictionary = Dictionary([["<UNK>"]])
+    dictionary.add_documents(train_documents)
 
-    known_words = set(mydict.values())
+    known_words = set(dictionary.values())
 
+    return dictionary, known_words
+
+
+def doc2bow(doc, dictionary, known_words):
     def word_is_known(word):
         return word in known_words
 
     def filter_unknowns_in_doc(doc):
         return [word if word_is_known(word) else "<UNK>" for word in doc]
 
+    doc = filter_unknowns_in_doc(doc)
+    bag_of_words = dictionary.doc2bow(doc)
+    return bag_of_words
+
+
+def bow2vec(bag_of_words, known_words):
+    # the last 2 slots are for <UNK> and <NEVER OCCURS>
+    features_vector = np.zeros(len(known_words) + 2, dtype=np.uint16)
+    for word_id, count in bag_of_words:
+        features_vector[word_id] = count
+    return features_vector
+
+
+def test_csv_to_pickle(test_filename, pickle_filename, dictionary, known_words):
     X = []
     Y = []
 
-    print("    counting number of rows")
-    num_rows = sum(1 for line in open(in_file))
+    print("loading csv into memory")
+    df = pd.read_csv(test_filename, header=0)
+    num_rows = df.shape[0]  # sum(1 for line in open(test_filename))
 
-    print("    converting to feature representation")
+    print("converting to feature representation")
     for i, row in tqdm(df.iterrows(), total=num_rows):
         doc = row['text'].split()
         subreddit = row['subreddit']
 
-        doc = filter_unknowns_in_doc(doc)
-        features_bow = mydict.doc2bow(doc)
-
-        # the last 2 slots are for <UNK> and <NEVER OCCURS>
-        features_vector = np.zeros(len(known_words) + 2, dtype=np.uint16)
-        for word_id, count in features_bow:
-            features_vector[word_id] = count
+        bag_of_words = doc2bow(doc, dictionary, known_words)
+        features_vector = bow2vec(bag_of_words, known_words)
 
         X.append(features_vector)
         Y.append(subreddit)
 
-    print("    converting to numpy arrays")
+    print("converting to numpy arrays")
     X = np.array(X)
     Y = np.array(Y)
 
-    print("    saving file")
-    logistics_pickler.save_obj((X, Y), out_file, print_debug_info=False)
+    logistics_pickler.save_obj((X, Y), pickle_filename)
 
 
 def main():
-    apply_feature_representation("../res/data_sample.csv", "../pickle_files/feature_sample_bow.pkl")
-    apply_feature_representation("../res/data_all.csv", "../pickle_files/feature_all_bow.pkl")
-    apply_feature_representation("../res/data_all_train.csv", "../pickle_files/feature_all_train_bow.pkl")
-    apply_feature_representation("../res/data_all_test.csv", "../pickle_files/feature_all_test_bow.pkl")
-    apply_feature_representation("../res/data_all_validate.csv", "../pickle_files/feature_all_validate_bow.pkl")
+    dictionary, known_words = make_dictionary_and_known_words("../res/data_all_train.csv")
+    logistics_pickler.save_obj(dictionary, "feature_bow_dictionary.pkl")
+
+    test_csv_to_pickle("../res/data_all_train.csv", "../pickle_files/feature_all_train_bow.pkl", dictionary, known_words)
+    test_csv_to_pickle("../res/data_all_test.csv", "../pickle_files/feature_all_test_bow.pkl", dictionary, known_words)
+    test_csv_to_pickle("../res/data_all_validate.csv", "../pickle_files/feature_all_validate_bow.pkl", dictionary, known_words)
 
 
 if __name__ == "__main__":
