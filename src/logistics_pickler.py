d5382646e19398dea4966b229bc5db0e6742a394
Alex Lamson <alex.lamson@gmail.com>
2017-11-16 12:16:38 -0500

    made pickler debug prints more true :flower_playing_cards:

--- src/logistics_pickler.py
+++ src/logistics_pickler.py
@@ -13,6 +13,7 @@ def save_obj(obj, name, print_debug_info=True):
 def load_obj(name, print_debug_info=True):
     sanitized_name = name.replace('.pkl', '')
     with open('../pickle_files/' + sanitized_name + '.pkl', 'rb') as f:
+        obj = pickle.load(f)
         if print_debug_info:
             print('Loaded {}'.format(sanitized_name + '.pkl'))
-        return pickle.load(f)
+        return obj
