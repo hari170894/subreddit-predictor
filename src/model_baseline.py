cc99b105b84b406bafa9f99daa6360db80d0684a
Alex Lamson <alex.lamson@gmail.com>
2017-11-18 21:31:23 -0500

    made models use __main__ to make them accessible to logistics_main.py :purple_heart:

--- src/model_baseline.py
+++ src/model_baseline.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python2
 from __future__ import nested_scopes, generators, division, absolute_import, with_statement, print_function, unicode_literals
 import logistics_pickler
-from sklearn.metrics import accuracy_score
 
-print("loading pickled feature representation")
-X_train, Y_train = logistics_pickler.load_obj("../pickle_files/features_all_train_bow.pkl")
-X_test, Y_test = logistics_pickler.load_obj("../pickle_files/features_all_test_bow.pkl")
 
-print("training classifier")
-most_common_class = max(set(Y_train), key=list(Y_train).count)
+def main():
+    print("loading pickled feature representation")
+    X_train, Y_train = logistics_pickler.load_obj("../pickle_files/features_all_train_bow.pkl")
+    X_test, Y_test = logistics_pickler.load_obj("../pickle_files/features_all_test_bow.pkl")
 
-print("evaluating model")
-y_true = Y_test
-y_pred = [most_common_class] * len(y_true)
-accuracy = accuracy_score(y_true, y_pred)
-print("accuracy: {}".format(accuracy))
+    print("training classifier")
+    most_common_class = max(set(Y_train), key=list(Y_train).count)
+
+    print("evaluating model")
+    y_true = Y_test
+    y_pred = [most_common_class] * len(y_true)
+    logistics_pickler.save_obj((y_true, y_pred), "predictions_baseline.pkl")
+
+
+if __name__ == "__main__":
+    main()
--- src/model_naive_bayes.py
+++ src/model_naive_bayes.py
@@ -6,8 +6,8 @@ from sklearn.naive_bayes import MultinomialNB
 
 def create_predictions(train_file, test_file, output_file):
     print("loading pickled feature representation")
-    T_train,X_train, Y_train = logistics_pickler.load_obj(train_file)
-    T_test,X_test, Y_test = logistics_pickler.load_obj(test_file)
+    T_train, X_train, Y_train = logistics_pickler.load_obj(train_file)
+    T_test, X_test, Y_test = logistics_pickler.load_obj(test_file)
 
     print("training classifier")
     clf = MultinomialNB()
@@ -20,10 +20,15 @@ def create_predictions(train_file, test_file, output_file):
     logistics_pickler.save_obj((y_true, y_pred), output_file)
 
 
-create_predictions("features_all_train_bow.pkl",
-                   "features_all_validate_bow.pkl",
-                   "predictions_bow_naive_bayes.pkl")
+def main():
+    create_predictions("features_all_train_bow.pkl",
+                       "features_all_validate_bow.pkl",
+                       "predictions_bow_naive_bayes.pkl")
 
-# create_predictions("features_train_word2vec_sum.pkl",
-#                    "features_all_validate_bow.pkl",
-#                    "predictions_bow_naive_bayes.pkl")
+    # create_predictions("features_train_word2vec_sum.pkl",
+    #                    "features_all_validate_bow.pkl",
+    #                    "predictions_bow_naive_bayes.pkl")
+
+
+if __name__ == "__main__":
+    main()
