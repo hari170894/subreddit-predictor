d2612018a4f3cfff6cc2ea36a69ea4bb62fa61bc
Alex Lamson <alex.lamson@gmail.com>
2017-11-18 20:25:43 -0500

    generalized the way naive bayes gets features :bomb:

--- src/model_naive_bayes.py
+++ src/model_naive_bayes.py
@@ -2,18 +2,24 @@
 from __future__ import nested_scopes, generators, division, absolute_import, with_statement, print_function, unicode_literals
 import logistics_pickler
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
 
-print("loading pickled feature representation")
-X_train, Y_train = logistics_pickler.load_obj("../pickle_files/feature_all_train_bow.pkl")
-X_test, Y_test = logistics_pickler.load_obj("../pickle_files/feature_all_test_bow.pkl")
 
-print("training classifier")
-clf = MultinomialNB()
-clf.fit(X_train, Y_train)
+def create_predictions(train_file, test_file, output_file):
+    print("loading pickled feature representation")
+    X_train, Y_train = logistics_pickler.load_obj(train_file)
+    X_test, Y_test = logistics_pickler.load_obj(test_file)
 
-print("evaluating model")
-y_true = Y_test
-y_pred = clf.predict(X_test)
-accuracy = accuracy_score(y_true, y_pred)
-print("accuracy: {}".format(accuracy))
+    print("training classifier")
+    clf = MultinomialNB()
+    clf.fit(X_train, Y_train)
+
+    print("evaluating model")
+    y_true = Y_test
+    y_pred = clf.predict(X_test)
+
+    logistics_pickler.save_obj((y_true, y_pred), output_file)
+
+
+create_predictions("feature_all_train_bow.pkl",
+                   "feature_all_validate_bow.pkl",
+                   "predictions_bow_naive_bayes.pkl")
