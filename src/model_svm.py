8273b983b5f71986a570505452793019792c45a7
Alex Lamson <alex.lamson@gmail.com>
2017-11-18 21:56:20 -0500

    added most of code to svm model :v:

---
+++ src/model_svm.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python2
+from __future__ import nested_scopes, generators, division, absolute_import, with_statement, print_function, unicode_literals
+import logistics_pickler
+from sklearn.svm import SVC
+
+
+def create_predictions(train_file, test_file, output_file):
+    print("loading pickled feature representation")
+    T_train, X_train, Y_train = logistics_pickler.load_obj(train_file)
+    T_test, X_test, Y_test = logistics_pickler.load_obj(test_file)
+
+    print("training classifier")
+    clf = SVC()
+    clf.fit(X_train, Y_train)
+
+    print("evaluating model")
+    y_true = Y_test
+    y_pred = clf.predict(X_test)
+
+    logistics_pickler.save_obj((T_test, y_true, y_pred), output_file)
+
+
+def main():
+    print("training SVM")
+
+    create_predictions("features_all_train_bow.pkl",
+                       "features_all_validate_bow.pkl",
+                       "predictions_bow_svm.pkl")
+
+    create_predictions("features_train_word2vec_sum.pkl",
+                       "features_validation_word2vec_sum.pkl",
+                       "predictions_word2vec_naive_bayes.pkl")
+
+    create_predictions("features_train_doc2vec_sum.pkl",
+                       "features_validation_doc2vec_sum.pkl",
+                       "predictions_doc2vec_svm.pkl")
+
+
+if __name__ == "__main__":
+    main()
